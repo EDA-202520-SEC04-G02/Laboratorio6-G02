1c0a4643eebaaf342542d1b70088dbd1063e100f
LuigiToby <la.rodrigueza12@uniandes.edu.co>
2025-10-01 15:37:22 -0500

    actualizar data structures

--- App/view.py
+++ App/view.py
@@ -27,7 +27,9 @@
 import sys
 import App.logic as logic
 # TODO Realice la importación del mapa linear probing
+from DataStructures.Map import map_linear_probing as lp
 # TODO Realice la importación de ArrayList como estructura de datos auxiliar para sus requerimientos
+from DataStructures.List import array_list as al
 
 
 """
